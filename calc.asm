e80e52ee935c66e04990bc939ce488cedbe7c615
isacoelhou <isadora.araujo1@unioeste.br>
2023-07-10 08:35:57 -0300

    func

--- calc.asm
+++ calc.asm
@@ -2,12 +2,13 @@ extern printf
 extern scanf
 extern fopen
 extern fclose
+; nasm -f elf64 calc.asm ; gcc -m64 -no-pie calc.o -o calc.x
 
 section .data
     ola: db "Equação :", 10, 0
     erro: db "funcionalidade não disponível", 10
     vart: db "%f %c %f", 0
-    modo: "a", 0
+    modo: db "a", 0
     nomearq: db "respostas.txt", 0
 
 section .bss
@@ -20,32 +21,45 @@ section .text
     global main
 
 soma:
-    mov xmm0, op1
-    mov xmm1, op2
-    addss xmm0, xmm1  ; Adicionar xmm1 a xmm0
+    movss xmm0, dword[op1]   ; Carregar op1 em xmm0
+    movss xmm1, dword[op2]   ; Carregar op2 em xmm1
+    addss xmm0, xmm1  
     ret
 
 menos:
-    mov xmm0, op1
-    mov xmm1, op2
+    movss xmm0, dword[op1]   
+    movss xmm1, dword[op2]   
     subss xmm0, xmm1 
     ret
 
+mult:
+    movss xmm0, dword[op1]   
+    movss xmm1, dword[op2]   
+    mulss xmm0, xmm1 
+k:
+    ret
+
+divide:
+    movss xmm0, dword[op1]   
+    movss xmm1, dword[op2]  
+    divss xmm0, xmm1 
+y:
+    ret
 
 arquivo:
     ;abre o arquivo
     lea rsi, [modo]
-    lea rdi, [nome]
+    lea rdi, [nomearq]
     call fopen
 
     mov [arq], eax 
 
     ;fecha
-    lea rdi, [nome]
+    lea rdi, [nomearq]
     call fclose
     jmp fim
 
-erro:
+errou:
     xor rax, rax
     mov rdi, erro
     mov esi, 1
@@ -73,30 +87,29 @@ main:
 
     ;move o char para r8b (por causa da compatibilidade do tamanho)
     mov r8b, [op]
-
     ;compara com as opções
-    cmp r8b, 's'
-    call menos ; ta errado, tem que fazer funções não labels
-    jmp arquivo 
+    cmp r8b, 's' 
+    call menos 
+    jmp fim 
 
 
     cmp r8b, 'a'
     call soma
-    jmp arquivo 
+    jmp fim 
 
     cmp r8b, 'm'
-    call mult ; ta errado, tem que fazer funções não labels
-    jmp arquivo 
+    call mult 
+    jmp fim 
 
 
     cmp r8b, 'd'
-    call divide ; ta errado, tem que fazer funções não labels
-    jmp arquivo 
+    call divide 
+    jmp fim 
 
 
-    cmp r8b, 'e'
-    call exp ; ta errado, tem que fazer funções não labels
-    jmp arquivo 
+    ;cmp r8b, 'e'
+    ;call exp 
+    ;jmp arquivo 
 
 
 fim:
