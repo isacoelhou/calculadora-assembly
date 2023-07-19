56d252d477833bd109a786a306913eeab74d3f1e
Leo-Calsavara <calsavara2511@gmail.com>
2023-07-19 16:17:45 -0300

    funções corrigidas, faltando escrita no arquivo

--- calc.asm
+++ calc.asm
@@ -10,10 +10,15 @@ extern fclose
 
 section .data
     ola: db "Equação :", 10, 0
-    erro: db "funcionalidade não disponível", 10
+    erro: db "funcionalidade não disponível", 10, 0
     vart: db "%f %c %f", 0
     modo: db "a", 0
     nomearq: db "respostas.txt", 0
+    zerof: dd 0.0
+    umf: dd 1.0
+    doisf: dd 2.0
+    strcontrol: db "%lf", 10, 0
+    
 
 section .bss
     arq: resd 1 
@@ -46,7 +51,7 @@ k:
 divide:
     movss xmm0, dword[op1]   
     movss xmm1, dword[op2]  
-    cmp xmm1, 0
+    comiss xmm1, xmm5
     je erro
 
     divss xmm0, xmm1 
@@ -56,45 +61,73 @@ exp:
     movss xmm0, dword[op1]   
     movss xmm1, dword[op2] 
     movss xmm2, dword[op1]
+    movss xmm5, dword[zerof]
+    movss xmm6, dword[umf]
+    movss xmm7, dword[doisf]
 
-    cmp xmm1, 0
-    jb erro
+    comiss xmm1, xmm5
+    jb errou
         
     ; if op2 == 0 return 1 
     ; se op2 = 0 ret 1 
-    cmp xmm1, 0
-        ret 1
-    
-    cmp xmm1, 1
-        ret xmm0
+    comiss xmm1, xmm5
+    je zero
+
+
+    comiss xmm1, xmm6
+    je igual
 
 
     conta:
-        mov r12, 2 
         mulss xmm0, xmm2
-        cmp xmm1, r12
+        comiss xmm1, xmm7
         jb conta
         
     e:
         ret
+    
+    igual:
+        movss xmm0, xmm1
+        ret
+    
+    zero:
+        movss xmm0, xmm5
+        ret
+    um: 
+        movss xmm0, xmm6
+        ret
+    
 
 
-arquivo:
+
+;arquivo:
     ;abre o arquivo
-    lea rsi, [modo]
-    lea rdi, [nomearq]
-    call fopen
+ ;   lea rsi, [modo]
+  ;  lea rdi, [nomearq]
+   ; call fopen
 
-  
+    ;xor rax, rax
+    ;mov rdi, ola
+    ;mov esi, 1
+    ;call fprintf    
+
     ;fecha
-    lea rdi, [nomearq]
-    call fclose
+    ;lea rdi, [nomearq]
+    ;call fclose
+    ;jmp fim
+
+
+printtela:
+    cvtss2sd xmm0, xmm0
+    mov rax, 1
+    mov rdi, strcontrol
+    call printf
     jmp fim
 
 errou:
     xor rax, rax
     mov rdi, erro
-    mov esi, 1
+    mov rsi, 1
     call printf
     jmp fim
 
@@ -117,31 +150,55 @@ main:
     lea rcx, [op2]
     call scanf
 
+l1:
     ;move o char para r8b (por causa da compatibilidade do tamanho)
     mov r8b, [op]
     ;compara com as opções
     cmp r8b, 's' 
-    je menos 
-    jmp arquivo
+    je callmenos 
+
 
     cmp r8b, 'a'
-    je soma
-    jmp arquivo
+    je callsoma
+
 
     cmp r8b, 'm'
-    je mult 
-    jmp arquivo
+    je callmult 
+
 
     cmp r8b, 'd'
-    je divide 
-    jmp arquivo
+    je calldivide 
 
     cmp r8b, 'e'
-    je exp 
-    jmp arquivo 
+    je callexp 
+ 
 
 
 fim:
+    mov rsp, rbp    
+    pop rbp
+
     mov rax, 60  
     xor edi, edi
     syscall
+
+
+calldivide:
+    call divide
+    jmp printtela
+
+callexp:
+    call exp
+    jmp printtela
+
+callmenos:
+    call menos
+    jmp printtela
+
+callmult:
+    call mult
+    jmp printtela
+
+callsoma:
+    call soma
+    jmp printtela
